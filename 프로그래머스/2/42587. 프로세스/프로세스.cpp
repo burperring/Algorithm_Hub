d0488f86c0b9ab894b35ae9ac030a30a89d67520
burperring <91236428+burperring@users.noreply.github.com>
2024-12-24 11:06:56 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    vector<int> q = priorities;
+    
+    sort(priorities.begin(), priorities.end(), greater<int>());
+    
+    while(1){
+        if(q[0] == priorities[0]){
+            q.erase(q.begin());
+            priorities.erase(priorities.begin());
+            answer++;
+            if(location == 0) return answer;
+            else location--;
+        }
+        else{
+            q.push_back(q[0]);
+            q.erase(q.begin());
+            location--;
+            if(location < 0) location = q.size() - 1;
+        }
+    }
+    
+    return answer;
+}
