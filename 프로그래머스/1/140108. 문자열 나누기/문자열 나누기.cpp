9745d20bec162cee66776bb7b24b91479f5e6c82
burperring <91236428+burperring@users.noreply.github.com>
2024-12-27 09:06:20 +0900

    [level 1] Title: 문자열 나누기, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    int X = 1;
+    int notX = 0;
+    int pivot = 0;
+    int num = 1;
+    
+    while(pivot < s.length()){
+        if(s[num] == s[pivot])
+            X++;
+        else
+            notX++;
+        
+        if(X == notX){
+            pivot = num + 1;
+            num++;
+            X = 1;
+            notX = 0;
+            answer++;
+        }
+        
+        num++;
+        
+        
+    }
+    
+    return answer;
+}
