031a0350b3f3ae6a366edda871444a5583ee48b5
burperring <91236428+burperring@users.noreply.github.com>
2025-01-15 09:03:29 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    string buff;
+    istringstream iss(s);
+    vector<int> v;
+    
+    while(getline(iss, buff, ' ')){
+        v.push_back(stoi(buff));
+    }
+    
+    sort(v.begin(), v.end());
+    
+    answer += to_string(v.front());
+    answer += " ";
+    answer += to_string(v.back());
+    
+    return answer;
+}
