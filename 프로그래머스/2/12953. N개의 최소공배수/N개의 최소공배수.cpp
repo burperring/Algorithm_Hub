4ff19f12afe414d4db45753fa663f2b774d57937
burperring <91236428+burperring@users.noreply.github.com>
2025-01-22 09:20:36 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int gcd(int a, int b){
+    int A = max(a, b);
+    int B = min(a, b);
+    
+    while(A % B != 0){
+        int r = A % B;
+        A = B;
+        B = r;
+    }
+    
+    return B;
+}
+
+int solution(vector<int> arr) {
+    int answer = arr[0];
+    
+    for(int i = 1; i < arr.size(); i++){
+        int GCD = gcd(answer, arr[i]);
+        int LCM = answer * arr[i] / GCD;
+        answer = LCM;
+    }
+    
+    return answer;
+}
