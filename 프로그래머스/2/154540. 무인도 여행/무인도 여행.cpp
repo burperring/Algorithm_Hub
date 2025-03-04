df6336391e2c3d5af8fd380b95c3589362fb2249
burperring <91236428+burperring@users.noreply.github.com>
2025-03-04 09:37:06 +0900

    [level 2] Title: 무인도 여행, Time: 1.62 ms, Memory: 4.71 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+vector<string> v;
+
+int maze(int x, int y){
+    queue<pair<int, int>> q;
+    q.push(make_pair(x, y));
+    int sum = v[x][y] - '0';
+    v[x][y] = 'X';
+    
+    while(!q.empty()){
+        int qx = q.front().first;
+        int qy = q.front().second;
+        
+        q.pop();
+        
+        for(int i = 0; i < 4; i++){
+            int mx = qx + dx[i];
+            int my = qy + dy[i];
+            
+            if(mx < 0 || mx >= v.size() || my < 0 || my >= v[0].length())
+                continue;
+            else if(v[mx][my] == 'X')
+                continue;
+            else{
+                sum += v[mx][my] - '0';
+                v[mx][my] = 'X';
+                q.push(make_pair(mx, my));
+            }
+        }
+    }
+    
+    return sum;
+}
+
+vector<int> solution(vector<string> maps) {
+    vector<int> answer;
+    v = maps;
+    
+    for(int i = 0; i < v.size(); i++){
+        for(int j = 0; j < v[i].length(); j++){
+            if(v[i][j] != 'X'){
+                int num = maze(i, j);
+                answer.push_back(num);
+            }
+        }
+    }
+    
+    if(answer.empty())
+        answer.push_back(-1);
+    else
+        sort(answer.begin(), answer.end());
+    
+    return answer;
+}
