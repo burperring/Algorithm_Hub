8a0c22546623b9e2d5061dd73f7ae24ccd427bbd
burperring <91236428+burperring@users.noreply.github.com>
2025-03-11 10:32:16 +0900

    [level 2] Title: 호텔 대실, Time: 0.43 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -21,12 +21,15 @@ int solution(vector<vector<string>> book_time) {
     
     priority_queue<int> q;
     for(int i = 0; i < v.size(); i++){
+        // v[i].first(입장시간)이 q.top 보다 크면
         if(!q.empty() && -q.top() <= v[i].first)
-            q.pop();
+            q.pop(); // 동일한 방 사용이 가능하므로 pop을 통해 제거
         
+        // -붙여서 push 하는 이유 (priority_queue 특성 때문에)
         q.push(-v[i].second);
     }
     
+    // 큐의 사이즈가 최소로 사용하는 방의 개수
     answer = q.size();
     
     return answer;
