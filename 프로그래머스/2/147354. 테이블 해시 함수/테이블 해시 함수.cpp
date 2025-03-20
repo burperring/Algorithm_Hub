02d4919361c46cbd493ac4af57cf82e31e76937c
burperring <91236428+burperring@users.noreply.github.com>
2025-03-20 09:18:19 +0900

    [level 2] Title: 테이블 해시 함수, Time: 13.77 ms, Memory: 57.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int iCol;
+
+bool cmp(vector<int> v1, vector<int> v2){
+    if(v1[iCol - 1] == v2[iCol - 1]) return v1[0] > v2[0];
+    return v1[iCol - 1] < v2[iCol - 1];
+}
+
+int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
+    int answer = 0;
+    vector<int> v;
+    iCol = col;
+    sort(data.begin(), data.end(), cmp);
+    
+    for(int i = row_begin - 1; i < row_end; i++){
+        int num = 0;
+        for(auto x : data[i]){
+            num += (x % (i + 1));
+        }
+        v.push_back(num);
+    }
+    
+    for(auto x : v){
+        answer = answer ^ x;
+    }
+        
+    return answer;
+}
