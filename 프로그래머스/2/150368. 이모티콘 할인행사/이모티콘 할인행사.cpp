b4fbdd91cfd7080d80d4a168d3b454639f123258
burperring <91236428+burperring@users.noreply.github.com>
2025-04-25 09:55:50 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 106.92 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -6,13 +6,11 @@ int dis[4] = {40,30,20,10};//할인율
 vector<pair<int,int>> price(7,{0,0});//할인된 가격, 할인율
 vector<int> answer(2,0);
 
-void    dfs(int cnt, int n, vector<vector<int>> users, vector<int> emoticons)
-{
+void dfs(int cnt, int n, vector<vector<int>> users, vector<int> emoticons){
     if(cnt == n)// 이모티콘에 대한 할인가격 할인율을 다 기입했을 때
     {
         int plus = 0, sum = 0;
-        for(int i=0;i<users.size();i++)
-        {
+        for(int i=0;i<users.size();i++){
             int tmp = 0;
             for(int j=0;j<n;j++)
                 if (users[i][0] <= price[j].second) tmp += price[j].first;//원하는 할인율이상이면 구매값++
@@ -27,16 +25,14 @@ void    dfs(int cnt, int n, vector<vector<int>> users, vector<int> emoticons)
         else if(plus == answer[0] && sum >= answer[1]) answer[1] = sum;// 가입자수가 같고 이윤이 크면
         return ;
     }
-    for(int i=0;i<4;i++)
-    {
+    for(int i=0;i<4;i++){
         price[cnt].first = (100 - dis[i]) * emoticons[cnt] / 100;//할인된 가격
         price[cnt].second = dis[i];//할인율
         dfs(cnt+1, n, users, emoticons);
     }
 }
 
-vector<int> solution(vector<vector<int>> users, vector<int> emoticons)
-{
+vector<int> solution(vector<vector<int>> users, vector<int> emoticons){
     dfs(0, emoticons.size(), users, emoticons);
     return answer;
 }
