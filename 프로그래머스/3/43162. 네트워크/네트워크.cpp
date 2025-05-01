4f1a9184987b00bfa9fe2d1a07f28456d910bddc
burperring <91236428+burperring@users.noreply.github.com>
2025-05-01 09:18:21 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -13,7 +13,8 @@ bool dfs(vector<vector<int>>& computers, int n) {
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     for (int i = 0; i < n; i++) {
-        if (computers[i][i] && dfs(computers, i))        answer++;
+        if (computers[i][i] && dfs(computers, i))
+            answer++;
     }
     return answer;
 }
